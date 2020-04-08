e63e75a9eced83df8b9e29a08c479517b90e3e1b
Pozadr <adpozor@gmail.com>
2020-04-08 17:47:51 +0200

    New class Money

---
+++ src/Money.java
@@ -0,0 +1,25 @@
+public class Money {
+    /*
+Napisz program, który:
+◦ będzie w pętli losowo pobierał wartość aż "uzbiera" wartość 100
+◦ każda pobrana wartość ma być wyświetlona
+◦ każda składowa suma ma być wyświetlona
+◦ na koniec ma być wyświetlona liczba pobrań pieniędzy koniecznych do uzbierania 100 oraz
+uzbierana suma (bo może przekroczyć 100)
+◦ spróbuj zmodyfikować ten program, tak by pozwalał uzbierać dokładnie zadaną kwotę (nie
+pozwalał na jej przekroczenie)
+     */
+    int[] money = {1, 2, 5, 10, 20};
+
+    public void displayCalculations(){
+        System.out.println("------------------MONEY------------------");
+        int goal = 100;
+        int counter = 0;
+        while(counter <= goal){
+            int idx = (int) (Math.random() * money.length + 1);
+            counter+= money[idx - 1];
+            System.out.print(counter + " ");
+        }
+
+    }
+}
